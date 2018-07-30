5f8ab8ea26be7d814410cc1919fc731961a8c77e
Rafael <rafaelsantos.rsdn@gmail.com>
2018-07-30 18:16:46 -0300

    Primeiro Commit

---
+++ Calculadora.Tests/CalculadoraTestes.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTestes
+    {
+        [TestFixture]
+        public class CalculadoraSimplesTests
+        {
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                //SUT = System Under Test
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Adicionar(5,5);
+                Assert.That(resultado, Is.EqualTo(10));
+            }
+
+            [Test]
+            public void DeveMultiplicarDoisNumeros()
+            {
+                //SUT = System Under Test
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Multiplicar(5, 3);
+                Assert.That(resultado, Is.EqualTo(15));
+            }
+
+            [Test]
+            public void DeveDividirDoisNumeros()
+            {
+                //SUT = System Under Test
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Dividir(6, 2);
+                Assert.That(resultado, Is.EqualTo(3));
+            }
+            [Test]
+            public void DeveSubtrairDoisNumeros()
+            {
+                //SUT = System Under Test
+                var sut = new CalculadoraSimples();
+                var resultado = sut.Subtrair(5, 3);
+                Assert.That(resultado, Is.EqualTo(2));
+            }
+
+        }
+    }
+}
---
+++ Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        /*Método para Adicionar 2 números*/
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /*Método para multiplicar 2 números*/
+        public int Multiplicar(int num1, int num2)
+        {
+            //Simular erro
+            return num1 * num2;
+        }
+
+        /*Método para multiplicar 2 números*/
+        public int Dividir(int num1, int num2)
+        {
+            //Simular erro
+            return num1 / num2;
+        }
+
+        /*Método para multiplicar 2 números*/
+        public int Subtrair(int num1, int num2)
+        {
+            //Simular erro
+            return num1 - num2;
+        }
+
+    }
+}
